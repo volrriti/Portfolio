897113c8db0ade233296e65a388ad553b029bd3e
volrriti <150788510+volrriti@users.noreply.github.com>
2023-11-15 16:18:27 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector("nav");
+const sectionOne = document.querySelector(".intersection");
+
+
+const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            nav.classList.add("nav-scrolled")
+        } else {
+            nav.classList.remove("nav-scrolled")
+        }
+    });
+});
+
+sectionOneObserver.observe(sectionOne)
